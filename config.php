f4ba27a38eba9fd13c2e8be2b2b19bf81b37a79b
Chris Wright <chrisw@networkm.co.uk>
2013-04-07 00:38:31 +0100

    Set cache timeout to 10 seconds
    Just testing, noting to see here

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
     $baseUrl = 'http://developer.android.com/reference/classes.html';
 
     $classCacheFile = __DIR__ . '/classes.xml';
-    $classCacheTTL = 14400;
+    $classCacheTTL = 10;
 
     $urlCacheFile = __DIR__ . '/urls.json';
