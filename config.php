0c873c50ff145a57ea54a95ff8cc1f93d48b90ac
Chris Wright <chrisw@networkm.co.uk>
2013-04-07 00:40:12 +0100

    Nothing to see here folks

--- config.php
+++ config.php
@@ -3,6 +3,6 @@
     $baseUrl = 'http://developer.android.com/reference/classes.html';
 
     $classCacheFile = __DIR__ . '/classes.xml';
-    $classCacheTTL = 10;
+    $classCacheTTL = 14400;
 
     $urlCacheFile = __DIR__ . '/urls.json';
